cb7f60231d5b2c8a445177e09c5a7b6085fbc812
snpr-grlgoku3 <khyatibagga4@gmail.com>
2025-05-20 16:29:55 +0530

    def square_and_filter(start, end):

---
+++ list.py
@@ -0,0 +1,9 @@
+def square_and_filter(start, end):
+    squares = [i ** 2 for i in range(start, end + 1)]
+    even_squares = [sq for sq in squares if sq % 2 == 0]
+    odd_squares = [sq for sq in squares if sq % 2 != 0]
+
+    print("Even square values:", even_squares)
+    print("Odd square values:", odd_squares)
+    
+square_and_filter(1, 10)
